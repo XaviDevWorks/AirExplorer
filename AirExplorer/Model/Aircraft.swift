38653fb1ae39db097066edf7e893ded826d9162d
XaviDevWorks <xfarmar@gmai.com>
2026-01-12 16:02:06 +0100

    feat: Model defined

---
+++ AirExplorer/Model/Aircraft.swift
@@ -0,0 +1,29 @@
+//
+//  model.swift
+//  AirExplorer
+//
+//  Created by alumne on 12/01/2026.
+//
+
+import Foundation
+
+struct Aircraft{
+    
+    let id = UUID()
+    let manufacturer: String
+    let model: String
+    let engineType: String
+    let engineThrustLbFt: String?
+    let maxSpeedKnots: String?
+    let cruiseSpeedKnots: String?
+    let ceilingFt: String?
+    let takeoffGroundRunFt: String?
+    let landingGroundRollFt: String?
+    let grossWeightLbs: String?
+    let emptyWeightLbs: String?
+    let lengthFt: String?
+    let heightFt: String?
+    let wingSpanFt: String?
+    let rangeNauticalMiles: String?
+    
+}
---
+++ AirExplorer/Model/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  AirExplorer
+//
+//  Created by alumne on 12/01/2026.
+//
+
+import Foundation
+
+
+struct User{
+    
+    let id = UUID() // ID único generadi automáticamente
+    let name: String // Nombre de usuario
+    let email: String  // Correo electrónico
+    let password: String // Contraseña
+    
+}
