9b82a5c5a98c7cdf85f55c7fdc8f28e6b945a058
XaviDevWorks <xfarmar@gmai.com>
2026-01-12 15:36:59 +0100

    Initial Commit

---
+++ AirExplorer/AirExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirExplorerApp.swift
+//  AirExplorer
+//
+//  Created by alumne on 12/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AirExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
